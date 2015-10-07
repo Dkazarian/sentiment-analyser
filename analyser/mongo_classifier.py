7519241d7b60a7b830a3a8e3a5fbe99c53bd91dc
Daniela Kazarian <dbkazarian@gmail.com>
2015-10-07 14:35:16 -0300

    lower case before searching

--- analyser/mongo_classifier.py
+++ analyser/mongo_classifier.py
@@ -3,6 +3,6 @@ from models.s_word import SWord
 
 class MongoClassifier(object):
   def classify(self, word):
-    s_word = SWords.find_word(word.string) or SWords.find_word(word.lemma) or SWord(word.string)
+    s_word = SWords.find_word(word.string.lower()) or SWords.find_word(word.lemma.lower()) or SWord(word.string.lower())
     s_word.extra = word 
     return s_word
