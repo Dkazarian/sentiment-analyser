f09b67f94e666f8eb9596edb5bae358d17a7a2cf
Aldana Quintana <aldanaqm@gmail.com>
2015-10-14 19:02:36 -0300

    Info route

--- api/analyser_server.py
+++ api/analyser_server.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 # coding: UTF-8
 import sys
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
   return render_template("index.html")
+
+@app.route("/info.json", methods=['GET'])
+def info():
+  return jsonify(
+    version="1.0",
+    year="2015",
+    authors=[
+      "Pablo Fernández",
+      "Daniela Kazarián",
+      "Aldana Laura Quintana Munilla",
+      "Ariel Umansky"
+    ]
+  )
 
 
 if __name__ == "__main__":
