f6d79234a0f846f4660ade8af489a826f85cfcb6
Aldana Quintana <aldanaqm@gmail.com>
2015-10-14 18:52:48 -0300

    Add template for index welcome

--- api/analyser_server.py
+++ api/analyser_server.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
-
+# coding: UTF-8
+import sys
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello flask world!"
+def index():
+  return render_template("index.html")
+
+
+if __name__ == "__main__":
+  app.run()
