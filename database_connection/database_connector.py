cdc0401f01eebf265cab251552775a51460cab9c
Daniela <dbkazarian@gmail.com>
2016-01-12 14:59:05 -0300

    Add missing file

---
+++ database_connection/database_connector.py
@@ -0,0 +1,21 @@
+class DatabaseConnector:
+  host = "localhost"
+  current_instance = None
+
+  def __init__(self, host, port, db_name):
+    self.init_client(host, port, db_name)
+    self.init_db(db_name)
+
+  @classmethod
+  def init_client(self, host, port):
+    raise NotImplementedError
+
+  @classmethod
+  def init_db(self, db_name):
+    raise NotImplementedError
+
+  @classmethod
+  def current(self):
+    if self.current_instance is None:
+      self.current_instance = self(self.host, self.port, self.db_name)
+    return self.current_instance
