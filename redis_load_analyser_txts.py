eb44342371bbe707af68ada67db78a4fdccab990
Daniela <dbkazarian@gmail.com>
2016-01-12 14:59:40 -0300

    commented ANEW in loader

--- redis_load_analyser_txts.py
+++ redis_load_analyser_txts.py
@@ -2,8 +2,8 @@
 import sys
 
 
-sys.argv = [sys.executable, "-p", "analyser/data/anew.txt"]
-execfile( "redis_words_loader.py")
+#sys.argv = [sys.executable, "-p", "analyser/data/anew.txt"]
+#execfile( "redis_words_loader.py")
 
 sys.argv = [sys.executable, "-m", "analyser/data/modifiers.txt"]
 execfile( "redis_words_loader.py")
